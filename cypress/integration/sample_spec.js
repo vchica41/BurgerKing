f60a77fe29f34a30ba62a91b2e9f6dcc8a5dea7b
vcolonlopez <vcolonlopez@gmail.com>
2021-11-17 11:59:39 -0500

    Added the rest of menu categories (/menu/section)

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -30,4 +30,59 @@ describe("My First Test", function () {
     clickButton("Featured");
     cy.url().should("include", "/menu/section");
   });
+
+  it("Go to sign up page", function () {
+    cy.visit("https://www.bk.com/");
+    clickButton("Sign Up");
+    cy.url().should("include", "/signup");
+  });
+
+  it("Go to Menu then click Flame Grilled Burgers button", function () {
+    cy.visit("https://www.bk.com/");
+    clickButton("Menu");
+    clickButton("Flame Grilled Burgers");
+    cy.url().should("include", "/menu/section");
+  });
+
+  it("Go to Menu then click Family Bundles button", function () {
+    cy.visit("https://www.bk.com/");
+    clickButton("Menu");
+    clickButton("Family Bundles");
+    cy.url().should("include", "/menu/section");
+  });
+
+  it("Go to Menu then click Chicken & More button", function () {
+    cy.visit("https://www.bk.com/");
+    clickButton("Menu");
+    clickButton("Chicken & More");
+    cy.url().should("include", "/menu/section");
+  });
+
+  it("Go to Menu then click Sides button", function () {
+    cy.visit("https://www.bk.com/");
+    clickButton("Menu");
+    clickButton("Sides");
+    cy.url().should("include", "/menu/section");
+  });
+
+  it("Go to Menu then click Drinks & Coffee button", function () {
+    cy.visit("https://www.bk.com/");
+    clickButton("Menu");
+    clickButton("Drinks & Coffee");
+    cy.url().should("include", "/menu/section");
+  });
+
+  it("Go to Menu then click Sweets button", function () {
+    cy.visit("https://www.bk.com/");
+    clickButton("Menu");
+    clickButton("Sweets");
+    cy.url().should("include", "/menu/section");
+  });
+
+  it("Go to Menu then click King Jr. button", function () {
+    cy.visit("https://www.bk.com/");
+    clickButton("Menu");
+    clickButton("King Jr.");
+    cy.url().should("include", "/menu/section");
+  });
 });
