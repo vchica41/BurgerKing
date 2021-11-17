862a1beec56cbaeb4ced7b22f67b8d5c9de1c400
vcolonlopez <vcolonlopez@gmail.com>
2021-11-17 02:53:33 -0500

    Added Menu/Signup tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,4 +2,30 @@ describe("My First Test", function () {
   it("Does not do much", function () {
     expect(true).to.equal(true);
   });
+
+  it("Go to menu page", function () {
+    cy.visit("https://www.bk.com/");
+
+    cy.wait(2000);
+
+    cy.contains("Menu", {
+      force: true,
+      timeout: 6000,
+    }).click();
+
+    cy.url().should("include", "/menu");
+  });
+
+  it("Go to sign up page", function () {
+    cy.visit("https://www.bk.com/");
+
+    cy.wait(2000);
+
+    cy.contains("Sign Up", {
+      force: true,
+      timeout: 6000,
+    }).click();
+
+    cy.url().should("include", "/signup");
+  });
 });
