218fe532f3e1b563569348d0397786dcfcf9b2e6
vcolonlopez <vcolonlopez@gmail.com>
2021-11-17 03:16:22 -0500

    Refactored click button into its own function

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,51 +1,33 @@
 describe("My First Test", function () {
+  function clickButton(whereTo) {
+    cy.wait(2000);
+
+    cy.contains(whereTo, {
+      force: true,
+      timeout: 6000,
+    }).click();
+  }
+
   it("Does not do much", function () {
     expect(true).to.equal(true);
   });
 
   it("Go to menu page", function () {
     cy.visit("https://www.bk.com/");
-
-    cy.wait(2000);
-
-    cy.contains("Menu", {
-      force: true,
-      timeout: 6000,
-    }).click();
-
+    clickButton("Menu");
     cy.url().should("include", "/menu");
   });
 
   it("Go to sign up page", function () {
     cy.visit("https://www.bk.com/");
-
-    cy.wait(2000);
-
-    cy.contains("Sign Up", {
-      force: true,
-      timeout: 6000,
-    }).click();
-
+    clickButton("Sign Up");
     cy.url().should("include", "/signup");
   });
 
   it("Go to Menu then click Featured button", function () {
     cy.visit("https://www.bk.com/");
-
-    cy.wait(2000);
-
-    cy.contains("Menu", {
-      force: true,
-      timeout: 6000,
-    }).click();
-
-    cy.wait(2000);
-
-    cy.contains("Featured", {
-      force: true,
-      timeout: 6000,
-    }).click();
-
+    clickButton("Menu");
+    clickButton("Featured");
     cy.url().should("include", "/menu/section");
   });
 });
